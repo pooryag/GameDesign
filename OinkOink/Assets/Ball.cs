97cee374f55c53ea977679edfb59c26c5b097b64
pooryag <poorya@gmail.com>
2018-11-11 20:31:06 +0100

    Oink! many features added
    -Throwing ball system implemented
    -sprites added
    - pinning the ball system added
    -prefabs made
    -scripts ball and spawn written
    
    the game at this stage has a ball, and you can throw it around and pin it by tapping on it mid air. Conveyor belt incoming for next installment.
    
    It's fun to just throw the ball around.

---
+++ OinkOink/Assets/Ball.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private bool isPressed = false;
+
+    public float timer = 0.15f;
+
+    public float maxDistance = 2.0f;
+
+    public Rigidbody2D hook;
+
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+
+        if (isPressed){
+
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+            if ( Vector3.Distance (hook.position, mousePos) > maxDistance){
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDistance;
+            }
+            else
+                rb.position = mousePos;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+       
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Unhook());
+    }
+
+    IEnumerator Unhook (){
+
+        yield return new WaitForSeconds(timer);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        this.enabled = false;
+    }
+
+}
---
+++ OinkOink/Assets/Spawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject ballHook;
+
+    private int count = 0;
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1")){
+            
+            if (count == 2)
+            {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                GameObject newAnchor = Instantiate(Resources.Load("BallHook", typeof(GameObject)), mousePos, Quaternion.identity) as GameObject;
+                count = 0;
+                Destroy(gameObject);
+            }
+
+
+
+        }
+    }
+    private void OnMouseDown()
+    {
+
+        count++;
+        //Debug.Log("I SAID WHAT? " + count);
+    }
+}
