686902c0d25ae071748131e2cc19e45666f7b06a
pooryag <poorya@gmail.com>
2018-11-29 01:08:47 +0100

    Redo button, new characters, floating text score, new scene

---
+++ OinkOink/Assets/ReloadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadCurrentScene(){
+        Spawn.ballCount = 3;
+        Spawn.count = 0; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- OinkOink/Assets/TextFade.cs
+++ OinkOink/Assets/TextFade.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class TextFade : MonoBehaviour {
     public float destroyTime = 3f;
+    //public Vector3 offset = new Vector3(0, 2, 0);
 	// Use this for initialization
 	void Start () {
         Destroy(gameObject, destroyTime);
+       //transform.localPosition += offset;
 	}
 	
 
