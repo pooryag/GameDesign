7a33ef6de3df820146ca985f5e9374dbd38e2815
pooryag <poorya@gmail.com>
2018-11-29 01:47:36 +0100

    Basic Win/lose, particle effects added

--- OinkOink/Assets/Drop.cs
+++ OinkOink/Assets/Drop.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Drop : MonoBehaviour {
 
-	// Use this for initialization
-	void Update () {
+    public GameObject deathParticle;
+    public GameObject winParticle;
+    public GameObject winText;
+    //private int count = 0;
+    // Use this for initialization
+    void Update () {
 		
         GetComponent<Rigidbody2D>().simulated |= Spawn.count == 2;
 	}
-	
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("wtf");
+        if (other.gameObject.tag == "Death")
+        {
+            Instantiate(deathParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        if (other.gameObject.tag == "Heaven")
+        {
+            //count++;
+            Instantiate(winParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+        }
+
+    }
+
 
 }
