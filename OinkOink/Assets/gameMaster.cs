336ec6eb951b3356cc6dae6669ab4258dbdf3e43
pooryag <poorya@gmail.com>
2018-12-17 04:56:20 +0100

    Winning stage with everything basically, need losing stage and music....jesus

--- OinkOink/Assets/gameMaster.cs
+++ OinkOink/Assets/gameMaster.cs
@@ -24,9 +24,50 @@ public class gameMaster : MonoBehaviour {
     {
 
 
-        yield return new WaitForSeconds(6.0f);
+        yield return new WaitForSeconds(5.0f);
         Debug.Log("WIN");
         completeLevelUI.SetActive(true);
+        if (points == 2) {
+            completeLevelUI.transform.Find("coin1").gameObject.SetActive(false);
+        }
+        if (points == 1)
+        {
+            completeLevelUI.transform.Find("coin1").gameObject.SetActive(false);
+            completeLevelUI.transform.Find("coin2").gameObject.SetActive(false);
+        }
+        if (points == 0)
+        {
+            completeLevelUI.transform.Find("coin1").gameObject.SetActive(false);
+            completeLevelUI.transform.Find("coin2").gameObject.SetActive(false);
+            completeLevelUI.transform.Find("coin3").gameObject.SetActive(false);
+        }
+        if (mom){
+            completeLevelUI.transform.Find("head1/head1_dead").gameObject.SetActive(false);
+        }else{
+            completeLevelUI.transform.Find("head1/head1").gameObject.SetActive(false);
+        }
+        if (kid)
+        {
+            completeLevelUI.transform.Find("head2/head2_dead").gameObject.SetActive(false);
+        }else{
+            completeLevelUI.transform.Find("head2/head2").gameObject.SetActive(false);
+        }
+        if (dad)
+        {
+            completeLevelUI.transform.Find("head3/head3_dead").gameObject.SetActive(false);
+        }
+        else
+        {
+            completeLevelUI.transform.Find("head3/head3").gameObject.SetActive(false);
+        }
+        if (son)
+        {
+            completeLevelUI.transform.Find("head4/head4_dead").gameObject.SetActive(false);
+        }
+        else
+        {
+            completeLevelUI.transform.Find("head4/head4").gameObject.SetActive(false);
+        }
 
 
     }
